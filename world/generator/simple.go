0a48747d4d3dd47d9ec86827010e6c295c310482
Thierry Geindre <thierry.geindre@bedrockstreaming.com>
2023-12-03 14:25:22 +0100

    Fix simple generator

--- world/generator/simple.go
+++ world/generator/simple.go
@@ -1,6 +1,7 @@
 package generator
 
 import (
+	"mycraft/world"
 	"mycraft/world/block"
 	"mycraft/world/generator/noise"
 )
@@ -18,7 +19,23 @@ func NewSimpleGenerator(baseNoise noise.Noise, waterLevel float32) *Simple {
 	return s
 }
 
-func (s *Simple) GetBlockAt(x, y, z float32) uint16 {
+func (s *Simple) Populate(chunk *world.Chunk) {
+	for x := float32(0); x < world.ChunkWidth; x++ {
+		for z := float32(0); z < world.ChunkWidth; z++ {
+			sampleX := x + chunk.Position().X
+			sampleZ := z + chunk.Position().Y
+			for y := float32(0); y < world.ChunkHeight; y++ {
+				if y == 0 {
+					chunk.SetBlockAtF(x, y, z, block.TypeBedrock)
+					continue
+				}
+				chunk.SetBlockAtF(x, y, z, s.getBlockAt(sampleX, y, sampleZ))
+			}
+		}
+	}
+}
+
+func (s *Simple) getBlockAt(x, y, z float32) uint8 {
 	groundLevel := s.noise.Eval2(x, z)
 	if y == groundLevel {
 		return block.TypeGrass
